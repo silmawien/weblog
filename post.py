44e77f046c8d2ebce86a65337f6f5e15be10b1e4
Mattias Niklewski <mattias@niklewski.com>
2013-07-11 12:01:11 +0200

    Better abstracts

--- post.py
+++ post.py
@@ -15,9 +15,6 @@ from urllib import quote
 from markdown import markdown
 import codecs
 
-# number of paragraphs in abstracts
-ABSTRACT_SIZE = 1
-
 
 def make_tag_link(tag):
     "Create href, display-name from a tag name."
@@ -51,19 +48,11 @@ def make_date(datestr):
             "htmltime": datestr, "isotime": iso_time(dt)}
 
 
-def paragraph_counter(num):
-    "Count empty lines and return False after num such lines."
-    count = [num] # workaround for strange scoping rules in python 2.x
-    done = [False] 
-    def counter(line):
-        line = line.strip()
-        if not line:
-            count[0] -= 1
-        # include trailing links, ie. [foo]: http://foo.com
-        if count[0] <= 0 and line and line[0] != '[':
-            done[0] = True
-        return not done[0]
-    return counter
+metatrans = {
+    "tags": lambda x: map(make_tag_link, tagsplit(x)),
+    "posted": lambda x: make_date(x),
+    "created": lambda x: make_date(x)
+}
 
 
 def read_post(src, dst):
@@ -73,34 +62,23 @@ def read_post(src, dst):
     dst is the web-root relative path to view a single post
     """
     post = dict()
-    with codecs.open(src, "r", "utf-8") as f:
-        for line in f:
-            # Metadata section ends with an empty line
-            if not line.strip(): break
-            # Metadata has the format "key: value"
-            key, value = re.split(u"\s*:\s*", line, 1)
-            key = key.lower()
-            value = value.strip()
+    with codecs.open(src, "r", "utf-8") as file:
+        contents = file.read()
+    meta, body = contents.split('\n\n', 1)
+    for m in meta.split('\n'):
+        key, value = re.split(u'\s*:\s*', m.strip(), 1)
+        key = key.lower()
+        if key in metatrans:
+            value = metatrans[key](value)
+        post[key] = value
 
-            # linkify tags "tags: tag1 tag2"
-            if key == "tags":
-                post[key] = map(make_tag_link, tagsplit(value))
-            elif key == "posted" or key == "created":
-                post[key] = make_date(value)
-            else:
-                post[key] = value
+    # self-link (for header and abstract [...])
+    post["url"] = dst
 
-        # self-link (for header and abstract [...])
-        post["url"] = dst
+    # Format with markdown
+    post["content"] = markdown(body)
 
-        # The remainder is markdown-content
-        body = [line for line in f]
-
-        # Format with markdown
-        md = "".join(body)
-        post["content"] = markdown(md)
-
-        # Make a nice abstract, also with markdown
-        abstract = "".join(takewhile(paragraph_counter(ABSTRACT_SIZE), body))
-        post["abstract"] = markdown(abstract)
+    # Make a nice abstract, also with markdown
+    abstract = body.split('<!-- abstract -->', 1)[0]
+    post["abstract"] = markdown(abstract)
     return post
